c06c6995c52a3a58de299b3c4c38af2895c72318
viz <visuweshmuthukumar@gmail.com>
2019-10-30 23:46:03 +0530

    add term.el

---
+++ term.el
@@ -0,0 +1,25 @@
+(use-package vterm
+  :config
+  (custom-set-faces
+   `(vterm-color-default ((t :foreground ,vz/color7 :background ,vz/color0)))
+   `(vterm-color-black   ((t :foreground ,vz/color7 :background ,vz/color0)))
+   `(vterm-color-red     ((t :foreground ,vz/color7 :background ,vz/color7)))
+   `(vterm-color-green   ((t :foreground ,vz/color7 :background ,vz/color7)))
+   `(vterm-color-yellow  ((t :foreground ,vz/color7 :background ,vz/color7)))
+   `(vterm-color-blue    ((t :foreground ,vz/color7 :background ,vz/color7)))
+   `(vterm-color-magenta ((t :foreground ,vz/color7 :background ,vz/color7)))
+   `(vterm-color-cyan    ((t :foreground ,vz/color7 :background ,vz/color7)))
+   `(vterm-color-white   ((t :foreground ,vz/color7 :background ,vz/color7))))
+
+  (defun vz/term-switch ()
+    (interactive)
+    (let ((vterm-buf (get-buffer "vterm")))
+      (if (member vterm-buf (buffer-list))
+          (switch-to-buffer-other-window vterm-buf)
+        (vterm-other-window))))
+
+  (general-define-key
+   :states  'normal
+   :keymaps 'override
+   :prefix  "SPC"
+   "T"      'vz/term-switch))
