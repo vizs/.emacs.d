deef8d5d7218a2b79937b828700c8fbbfdd9008e
viz <visuweshmuthukumar@gmail.com>
2019-11-19 23:24:56 +0530

    sane.el: add le funny comments

--- pkg.el
+++ pkg.el
@@ -3,6 +3,7 @@
 
 (when (version< emacs-version "27.0")
   (package-initialize))
+
 (require 'package)
 
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
@@ -15,13 +16,13 @@
 
 (require 'use-package)
 (require 'use-package-ensure)
-(setq use-package-always-ensure t
-      vc-follow-symlinks t)
+(setq use-package-always-ensure  t
+      vc-follow-symlinks         t)
 
 (defun vz/load-pkg (pkg-name)
   (let ((pkg-path (concat user-emacs-directory "/pkgs/" pkg-name)))
     (if (file-directory-p pkg-path)
-        (progn
-          (add-to-list 'load-path pkg-path)
-          t)
+	(progn
+	  (add-to-list 'load-path pkg-path)
+	  t)
       nil)))
--- sane.el
+++ sane.el
@@ -1,15 +1,22 @@
 ;; sane defaults
 
+;; remove unnecessary visual clutter
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (mouse-wheel-mode -1)
 (blink-cursor-mode 0)
 
+;; indentation and cursor
 (setq-default cursor-type      '(hbar . 3)
               tab-width         4
               indent-tabs-mode  nil)
 
+(defvaralias 'c-basic-offset     'tab-width)
+(defvaralias 'cperl-indent-level 'tab-width)
+(setq-default indent-tabs-mode nil)
+
+;; jesus fucking christ, emacs' backup system is a pain in the fucking arse
 (setq backup-by-copying               t
       backup-directory-alist         '(("." . "~/var/cache/emacs-bkups"))
       delete-old-versions             t
@@ -26,18 +33,20 @@
                                        (:eval (vz/mode-line-file-name)))
       gc-cons-threshold               50000000)
 
+;; i cba to type yes
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 (use-package ace-window)
+
+;; i hate when i touch my touchpad accidentally
 (use-package disable-mouse
   :config
   (global-disable-mouse-mode))
 
-(defvaralias 'c-basic-offset     'tab-width)
-(defvaralias 'cperl-indent-level 'tab-width)
-(setq-default indent-tabs-mode nil)
+;; i hate HATE when custom junk go to init.el
 
 (when (file-exists-p custom-file)
   (load custom-file))
 
+;; who likes trailing whitespaces anw?
 (add-hook 'before-save-hook 'whitespace-cleanup)
