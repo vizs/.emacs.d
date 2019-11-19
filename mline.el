328cba9e2212e07eeef8291cc4142f42de5a962b
viz <visuweshmuthukumar@gmail.com>
2019-11-19 22:41:47 +0530

    set header-line/awesome-tray according to var in init.el

--- mline.el
+++ mline.el
@@ -13,25 +13,24 @@
           (t "")))
   "")
 
-(setq vz/mode-line-format
-      '("  "
-        (:eval (vz/mode-line-file-name))
-        (:eval (vz/mode-line-file-state))
-        "     "
-        (line-number-mode   "L%l")
-        (column-number-mode " C%c" t)
-        "     "
-        mode-name))
+;; (setq vz/mode-line-format
+;;       '("  "
+;;         (:eval (vz/mode-line-file-name))
+;;         (:eval (vz/mode-line-file-state))
+;;         "     "
+;;         (line-number-mode   "L%l")
+;;         (column-number-mode " C%c" t)
+;;         "     "
+;;         mode-name))
+
+(setq vz/mode-line-format '((:eval (vz/mode-line-file-name))))
 
 (defun vz/set-mode-line-face ()
   (custom-set-faces
    `(header-line           ((t :background ,vz/color0 :foreground ,vz/color7)))
-   `(header-line-highlight ((t :background ,vz/color0 :foreground ,vz/color15)))
+   `(header-line-highlight ((t :background ,vz/color8 :foreground ,vz/color15)))
    `(mode-line             ((t :background ,vz/color0 :foreground ,vz/color7)))
    `(mode-line-highlight   ((t :background ,vz/color0 :foreground ,vz/color15)))))
-
-(setq-default mode-line-format   nil
-              header-line-format vz/mode-line-format)
 
 (defun vz/toggle-header-line ()
   (interactive)
@@ -45,26 +44,40 @@
       (setq mode-line-format vz/mode-line-format)
     (setq mode-line-format nil)))
 
-(when (vz/load-pkg "awesome-tray")
-  (require 'awesome-tray)
-  (defun vz/tray-line-column ()
-    (format "L%s C%s"
-            (format-mode-line "%l")
-            (format-mode-line "%c")))
 
-  (add-to-list 'awesome-tray-module-alist
-               '("vz/loc" .
-                 (vz/tray-line-column default)))
+(cond
+ ;; awesome-tray
+ ((= vz/tray 1)
+  (when (vz/load-pkg "awesome-tray")
+    (require 'awesome-tray)
 
-  (defun vz/tray-file-name ()
-    (format "%s%s"
-            (vz/mode-line-file-name)
-            (vz/mode-line-file-state)))
+    (defun vz/tray-line-column ()
+      (format-mode-line "   %p   L%l   C%c"))
 
-  (add-to-list 'awesome-tray-module-alist
-               '("vz/file" .
-                 (vz/tray-file-name default)))
+    (add-to-list 'awesome-tray-module-alist
+                 '("vz/loc" .
+                   (vz/tray-line-column default)))
 
-  (setq awesome-tray-active-modules '("vz/file" "vz/loc"))
-  (setq-default header-line-format nil)
-  (awesome-tray-mode 1))
+    (defun vz/tray-file-name ()
+      (format "%s%s"
+              (vz/mode-line-file-name)
+              (vz/mode-line-file-state)))
+
+    (add-to-list 'awesome-tray-module-alist
+                 '("vz/file" .
+                   (vz/tray-file-name default)))
+
+    (setq awesome-tray-active-modules '("vz/file" "vz/loc"))
+    (setq-default header-line-format nil
+                  mode-line-format   nil)
+    (awesome-tray-mode)))
+
+ ;; header-line
+ ((= vz/tray 2)
+  (setq-default header-line-format vz/mode-line-format
+                mode-line-format   nil))
+
+ ;; if anything else, don't display {mode,head}-line or awesome-tray
+ (t
+  (setq-default header-line-format nil
+                mode-line-format   nil)))
