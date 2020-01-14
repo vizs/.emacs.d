6195d44eba01bac524eaa1a5be1a92f29d8549ea
viz <visuweshmuthukumar@gmail.com>
2020-01-14 17:58:27 +0530

    win: add more edwina keybinds

--- win.el
+++ win.el
@@ -1,16 +1,28 @@
 (use-package edwina
   :config
-  (setq display-buffer-base-action '(display-buffer-below-selected))
+  (setq display-buffer-base-action '(display-buffer-below-selected)
+        edwina-mfact                0.60)
+  (defun vz/edwina-default-mfact ()
+    (interactive)
+    (setq edwina-mfact 0.60)
+    (edwina-arrange))
   (edwina-mode 1)
   (vz/bind-nois
-   "C-w O"     'delete-other-windows
-   "C-w o"     'ace-window
-   "C-w r"     'edwina-arrange
-   "C-w j"     'edwina-select-next-window
-   "C-w k"     'edwina-select-next-window
-   "C-w RET"   'edwina-zoom
-   "C-w S-RET" 'edwina-clone-window
-   "C-w q"     'edwina-delete-window))
+   "C-w o"   'ace-window
+   "C-w x"   'ace-delete-window
+   "C-w r"   'edwina-arrange
+   "C-w R"   'vz/edwina-default-mfact
+   "C-w j"   'edwina-select-next-window
+   "C-w k"   'edwina-select-previous-window
+   "C-w h"   'evil-window-left
+   "C-w l"   'evil-window-right
+   "C-w H"   'edwina-dec-mfact
+   "C-w L"   'edwina-inc-mfact
+   "C-w i"   'edwina-inc-nmaster
+   "C-w d"   'edwina-dec-nmaster
+   "C-w RET" 'edwina-zoom
+   "C-w s"   'edwina-clone-window
+   "C-w q"   'edwina-delete-window))
 
 (use-package avy
   :config
@@ -22,3 +34,11 @@
 (use-package ace-window
   :config
   (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l)))
+
+;; so i dont get lost
+(use-package beacon
+  :config
+  (beacon-mode 1)
+  (setq beacon-blink-when-window-scrolls           nil
+        beacon-blink-when-point-moves-horizontally nil
+        beacon-blink-when-point-moves-vertically   nil))
