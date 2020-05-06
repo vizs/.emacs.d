594856418605e5bf0ab6f3cf49c06cd4d0fa60f5
viz <visuwesh@tutanota.com>
2020-05-06 13:57:44 +0530

    add shell

---
+++ shell.el
@@ -0,0 +1,55 @@
+;; NOTE: * you can see if you are using comint-mode if you check INSIDE_EMACS
+;;       * man is significantly faster than woman
+
+(setq explicit-shell-file-name (or (getenv "SHELL") "/bin/sh"))
+
+;; Disable colours in shell-mode
+(setf ansi-color-for-comint-mode 'filter)
+(setq-default
+ shell-font-lock-keywords nil
+ comint-buffer-maximum-size 2000
+ shell-cd-regexp "cd\\|c")
+
+(defun vz/shell-mode-init ()
+  (setq
+   comint-process-echoes t
+   Man-notify-method 'quiet))
+
+(defun vz/comint-send-input (&optional start end)
+  "Send region if present, otherwise current line to current buffer's process"
+  (interactive "r")
+  (if (use-region-p)
+	  (comint-send-string
+	   (get-buffer-process (current-buffer))
+	   (concat (buffer-substring (or start (region-beginning))
+								 (or end (region-end)))
+			   "\n"))
+	(comint-send-input))
+  (when (evil-visual-state-p)
+	(evil-exit-visual-state)))
+
+;; Disable duplicates
+(dolist (h '(comint-truncate-buffer comint-watch-for-password-prompt))
+  `(add-hook 'comint-output-filter-functions ,h))
+(add-hook 'shell-mode-hook #'vz/shell-mode-init)
+
+;; TODO: consider changing this to comint-mode-map?
+(general-nmap
+  :keymaps 'shell-mode-map
+  "<RET>" 'vz/comint-send-input
+  :prefix "["
+  "w" 'comint-write-output
+  "d" 'comint-delete-output
+  "j" 'comint-next-prompt
+  "k" 'comint-previous-prompt
+  "c" 'comint-clear-buffer)
+
+(general-imap
+  :keymaps 'shell-mode-map
+  "C-c" 'comint-interrupt-subjob
+  "C-z" 'comint-stop-subjob
+  "C-l" 'comint-clear-buffer)
+
+(general-vmap
+  :keymaps 'shell-mode-map
+  "<RET>" 'vz/comint-send-input)
