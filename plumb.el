7428c417fcb369c63918be16be88dc3f4fe42393
viz <visuwesh@tutanota.com>
2020-05-17 11:13:55 +0530

    Add plumb.el

---
+++ plumb.el
@@ -0,0 +1,93 @@
+(setq
+ vz/plumb-video-exts '("mp4" "mkv" "webm" "gif")
+ vz/plumb-audio-exts '("mp3" "flac" "ogg")
+ vz/plumb-img-exts '("png" "jpg" "jpeg" "JPEG" "JPG"))
+
+(defun vz/plumb-download-url (url)
+  "Download the given url and return the place in which it is stored"
+  (let ((file-name (make-temp-name "/tmp/plumb.")))
+    (call-process "curl" nil nil 0 "-O" file-name url)))
+
+(defun vz/plumb-yt (string)
+  (start-process "plumb-yt" nil "mpv" string))
+
+;; TODO: * Open in emacs if file size isn't large
+(defun vz/plumb-img (path)
+  (call-process "meh" nil nil 0 path))
+
+(defun vz/plumb-pdf (path)
+  (call-process "zathura" nil nil 0 path))
+
+;; Maybe consider mime types too?
+(defun vz/plumb--file (ext path)
+  (pcase ext
+    ("txt" (view-file-other-window path))
+    (`(or ,@vz/img-exts) (vz/plumb-img path))
+    ("pdf" (vz/plumb-pdf path))))
+
+(defun vz/plumb-file (string)
+  (if (bound-and-true-p vz/term-mode)
+      (vz/plumb--file (replace-regexp-in-string "^.*\\." string) string)
+    (find-file string)))
+
+(defun vz/plumb-url (string)
+  (let* ((file-name (replace-regexp-in-string "^.*/" "" string))
+         (ext (replace-regexp-in-string "^.*\\." file-name))
+         (path nil))
+    (cond
+     ((or (string-empty-p file-name) (string= ext "html"))
+      (browse-url-chromium string))
+     ((member ext (append vz/plumb-video-exts
+                             vz/plumb-audio-exts))
+      (vz/plumb-yt string))
+     (:else
+      (setq-local path vz/plumb-download-url string)
+      (vz/plumb--file ext path)
+      (delete-file path)))))
+
+(defun vz/plumb-eval (string)
+  (pcase major-mode
+    ('python-mode (python-send-string string))
+    ;;('scheme-mode (vz/run-scheme)
+    ('emacs-lisp-mode (eval (read string)))
+    (- (shell-command string))))
+
+;; When using use-package, it errors -- void variable wand-helper:maybe-... 
+(when (vz/load-pkg "wand")
+  (setq wand:*rules*
+   (list
+    (wand:create-rule :match "^.*\\([0-9][a-z]+\\)"
+                      :capture :whole
+                      :action #'man)
+    (wand:create-rule :match "https?://youtube.com"
+                      :capture :whole
+                      :action #'vz/plumb-yt)
+    (wand:create-rule :match "https?://youtu.be"
+                      :capture :whole
+                      :action #'vz/plumb-yt)
+    (wand:create-rule :match "https?://"
+                      :capture :whole
+                      :action #'vz/plumb-url)
+    (wand:create-rule :match "file://"
+                      :capture :after
+                      :action #'vz/plumb-file)
+    (wand:create-rule :match "\\..+$"
+                      :capture :whole
+                      :action #'vz/plumb-file)
+    (wand:create-rule :match "\\$ "
+                      :capture :after
+                      :action #'shell-commmand)
+    (wand:create-rule :match ".*"
+                      :capture :whole
+                      :action #'vz/plumb-eval)
+    ))
+  (defun vz/plumb ()
+    (interactive)
+    (wand:execute (if (region-active-p)
+                      (buffer-substring-no-properties (region-beginning)
+                                                      (region-end))
+                    (ivy-thing-at-point))))
+  (general-define-key
+   :states '("normal" "visual")
+   :keymaps 'override
+   "|" 'vz/plumb))
