e1bda73d49366622e4b9a639c7b3bbefa90fabe4
viz <visuwesh@tutanota.com>
2020-07-29 18:15:57 +0530

    Add paredit

--- lisp/hive/editing.el
+++ lisp/hive/editing.el
@@ -10,6 +10,18 @@ on the position of the cursor."
     (when (eq point (point))
       (beginning-of-line))))
 
+(defun vz/backward-kill-word-or-kill-region (arg)
+  "Run `kill-region' if region is active or
+`backward-kill-word'."
+  (interactive "p")
+  (if (use-region-p)
+      (if paredit-mode
+          (paredit-kill-region (region-beginning) (region-end))
+        (kill-region (region-beginning) (region-end)))
+    (if paredit-mode
+        (paredit-backward-kill-word)
+      (backward-kill-word arg))))
+
 (defun vz/increase-number-at-point (arg)
   "Increase the number at point by ARG."
   (interactive "p")
@@ -28,13 +40,21 @@ on the position of the cursor."
  "C-a" #'vz/beginning-of-line
  "C-c M-+" #'vz/increase-number-at-point
  "C-c M--" #'vz/decrease-number-at-point
+ "C-w" #'vz/backward-kill-word-or-kill-region
+
  ;; electric-indent-mode is considered
  "C-j" #'newline
+
  ;; C-x z and C-x M-ESC are hard to press
  "C-." #'repeat
  "C->" #'repeat-complex-command
+
  ;; I never use Emacs in a terminal
- "C-z" #'zap-up-to-char)
+ "C-z" #'zap-up-to-char
+
+ ;; Swap `query-replace` and `query-replace-regexp`
+ "M-%" #'query-replace-regexp
+ "C-M-%" #'query-replace)
 
 (use-package expand-region
   :defer t
@@ -43,5 +63,9 @@ on the position of the cursor."
   :config
   (setq expand-region-contract-fast-key "S-SPC"))
 
-;; (use-package paredit
-;;   :defer t)
+(use-package paredit
+  :defer t
+  :hook ((emacs-lisp-mode
+          lisp-interaction-mode
+          racket-mode
+          scheme-mode) . paredit-mode))
