eb6ed7375056a374e772b72e335b0a8753a0ea70
viz <visuwesh@tutanota.com>
2020-11-20 18:40:03 +0530

    shell: Use hashtable for history tracking

--- lisp/hive/shell.el
+++ lisp/hive/shell.el
@@ -2,17 +2,14 @@
 
 ;; * Basic configuration
 ;; ** Set path to shell
-
 (setq explicit-shell-file-name (or (getenv "SHELL") "/bin/sh"))
 
 ;; ** Disable colours in shell-mode
-
 (setf ansi-color-for-comint-mode 'filter)
 (setq-default shell-font-lock-keywords nil
               comint-buffer-maximum-size 2000)
 
 ;; ** Track $PWD more effectively
-
 ;; from http://0x0.st/Hroa
 (defun shell-sync-dir-with-prompt (string)
   "Set prompt to `|Pr0mPT|${PWD}|...'"
@@ -26,94 +23,86 @@
     string))
 
 ;; ** Initialise
-
 (defun vz/shell-mode-init ()
   (shell-dirtrack-mode nil)
-  (add-hook 'comint-output-filter-function #'comint-truncate-buffer)
-  (add-hook 'comint-output-filter-function #'comint-watch-for-password-prompt)
+  (add-hook 'comint-output-filter-function
+            #'comint-truncate-buffer)
+  (add-hook 'comint-output-filter-function
+            #'comint-watch-for-password-prompt)
   (setq-local
    ;; comint-prompt-read-only t
    inhibit-field-text-motion t
    comint-process-echoes t)
   (setq Man-notify-method 'quiet)
-  (add-hook 'comint-preoutput-filter-functions #'shell-sync-dir-with-prompt nil t))
+  (add-hook 'comint-preoutput-filter-functions
+            #'shell-sync-dir-with-prompt nil t))
 
 (add-hook 'shell-mode-hook #'vz/shell-mode-init)
 
 ;; * Helper functions
 ;; ** Are we inside a shell?
-
-;; TODO: Maybe we shouldn't consider nix-shell shells?
-
 (defun vz/inside-shell? (&optional buffer)
   "Is the current ``active'' process a shell?"
   (let* ((buffer (or buffer (current-buffer)))
          (child  (process-running-child-p (get-buffer-process buffer))))
     (or (null child)
         (f-equal? (asoc-get (process-attributes child) 'comm)
-                   (f-filename explicit-shell-file-name)))))
+                  (f-filename explicit-shell-file-name)))))
+
+;; ** Ivy action for running commands from shell history
+(defun vz/shell-history--ivy-action (command)
+  (comint-delete-input)
+  (comint-send-string proc (concat command "\n"))
+  (comint-add-to-input-history command)
+  (vz/shell-history--write command))
 
 ;; * Shell history tracking
-
-;; TODO: Maybe track command run in nix-shell separately?
-
 ;; ** Variables
-
-(defvar vz/shell-history-cache-file (~ ".cache/mksh_history.el")
+(defvar vz/shell-history-cache-file (~ ".cache/shell_history.el")
   "Path to file to save elisp data about shell history")
 
 ;; ** Functions
-
 (defun vz/shell-history--get ()
-  "Get shell history."
-  (when (f-exists? vz/shell-history-cache-file)
-      (read-from-whole-string (f-read vz/shell-history-cache-file))))
+  "Get shell history hashtable."
+  (if (not (f-exists? vz/shell-history-cache-file))
+      (make-hash-table :test #'equal :size 10000)
+    (read-from-whole-string (f-read vz/shell-history-cache-file
+                                    'utf-8))))
 
-(defun vz/shell-history--write (string)
-  "Add STRING to `default-directory' entry."
-  (let* ((string  (s-trim (substring-no-properties string)))
-         (history (vz/shell-history--get))
-         (old     (asoc-get history default-directory '())))
-    (unless (s-blank? string)
-      (f-write (format "%S"
-                       (asoc-put! history default-directory
-                                  (cons string old)
-                                  t))
-               'utf-8 vz/shell-history-cache-file))))
+(defun vz/shell-history--write (entry &optional directory)
+  "Prepend ENTRY to DIRECTORY key's value."
+  (let ((entry (s-trim (substring-no-properties entry)))
+        (directory (or directory default-directory))
+        (ht (vz/shell-history--get))
+        ;; From straight.el
+        (print-level nil)
+        (print-length nil))
+    (unless (s-blank? entry)
+      (puthash directory
+               (cons entry (gethash directory ht))
+               ht)
+      (f-write (prin1-to-string ht) 'utf-8 vz/shell-history-cache-file))))
 
-(defun vz/shell-history--sort (cwd)
-  "Sort shell-history according to CWD."
-  (let ((history (vz/shell-history--get)))
-    (append (asoc-get history cwd '())
-            (-flatten (asoc-values (asoc-filter (fn (not (f-equal? <> cwd)))
-                                                history))))))
-
-(add-hook 'comint-input-filter-functions
-          (defun vz/shell-history-input-hook (string)
-            "Function to add to `comint-input-filter-functions'"
-            (when (and (eq major-mode 'shell-mode)
-                       (vz/inside-shell?))
-              (vz/shell-history--write string))
-            t))
+(defun vz/shell-history--sort (dir)
+  "Sort shell history according to DIR."
+  (let* ((ht (vz/shell-history--get))
+         (his (gethash dir ht)))
+    (dolist (d (hash-table-keys ht))
+      (unless (equal d dir)
+        (setq his (append his (gethash d ht)))))
+    his))
 
 (defun vz/shell-insert-from-hist ()
-  "Search for command in history and run it."
   (interactive)
-  (let ((process (get-buffer-process (current-buffer))))
-    (when (vz/inside-shell?)
-      (let* ((input  (comint-get-old-input-default))
-             (iinput (unless (s-blank? input) (concat "^" input)))
-             (cmd    (ivy-read "> "
-                               (vz/shell-history--sort default-directory)
-                               :initial-input iinput :sort nil)))
-        (unless (s-blank? input)
-          (comint-delete-input))
-        (comint-send-string process (concat cmd "\n"))
-        (comint-add-to-input-history cmd)))))
-;; (vz/term-minor-mode-set-title cmd)))))
+  (when-let ((proc (get-buffer-process (current-buffer)))
+             (_ (vz/inside-shell?)))
+    (ivy-read
+     "> " (vz/shell-history--sort default-directory)
+     :initial-input (comint-get-old-input-default)
+     :sort nil
+     :action #'vz/shell-history--ivy-action)))
 
 ;; * History from shell
-
 (defun vz/shell-shell-history ()
   "Returns current shell's history as a list"
   (split-string (f-read
@@ -127,21 +116,15 @@
 (defun vz/shell-insert-from-shell-hist ()
   "Search for command in shell history and run it"
   (interactive)
-  (let ((proc (get-buffer-process (current-buffer))))
-    (when (vz/inside-shell?)
-      (let* ((input (comint-get-old-input-default))
-             (init-input (unless (string-empty-p input) (concat "^" input)))
-             (cmd (ivy-read "> " (vz/shell-shell-history)
-                            :initial-input init-input :sort nil)))
-        (unless (s-blank? input)
-          (comint-delete-input))
-        (comint-send-string proc (concat cmd "\n"))
-        (comint-add-to-input-history cmd)
-        ;;(vz/term-minor-mode-set-title cmd)
-        (vz/shell-history--write cmd)))))
+  (when-let ((proc (get-buffer-process (current-buffer)))
+             (_ (vz/inside-shell?)))
+    (ivy-read
+     "> " (vz/shell-shell-history)
+     :initial-input (comint-get-old-input-default)
+     :sort nil
+     :action #'vz/shell-history--ivy-action)))
 
 ;; * Jump to directory alias
-
 (defun vz/shell-get-dir-alias ()
   (-map (fn (cadr (s-split "=" <>)))
         (-drop-last
@@ -173,12 +156,10 @@
 
 ;; * Popup a shell in `default-directory'
 ;; ** Variables
-
 (defvar vz/popup-shells nil
   "List of all shell buffers opened by vz/popup-shell")
 
 ;; ** Functions
-
 (defun vz/popup-shell--add (buffer &optional cwd)
   "Add BUFFER to vz/popup-shells and add a process sentinel.
 If CWD is non-nil, then cd to CWD."
@@ -231,7 +212,6 @@ to it. If nothing is found, create a new buffer"
           (vz/popup-shell--add (shell (vz/uniqify "*shell")) cwd)))))))
 
 ;; * Jump to prompt
-
 (defun vz/shell--get-prompts (prompts point)
   "Return all prompts as a propertized string"
   (save-excursion
@@ -259,7 +239,6 @@ to it. If nothing is found, create a new buffer"
 
 ;; * Emacs frame as terminal
 ;; ** Variables
-
 (define-minor-mode vz/term-minor-mode
   "Minor mode for binding C-d in *term* buffers")
 
@@ -272,7 +251,6 @@ to it. If nothing is found, create a new buffer"
   "Frames used by *term-asdf* buffers")
 
 ;; ** Functions
-
 (defun vz/term-minor-mode-sentinel (process output)
   "Process sentinel to auto kill associated buffer and frame in term-mode"
   (unless (process-live-p process)
@@ -321,7 +299,6 @@ term buffer associated with it"
 (add-hook 'delete-frame-functions #'vz/term-minor-mode-on-delete-frame)
 
 ;; * Keybindings
-
 (defun vz/shell-send-keysequence-to-process (key)
   "Send keysequence to current running process"
   (interactive "sPress keysequence:")
